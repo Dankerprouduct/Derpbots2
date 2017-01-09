5a5c81aacd4c9785d3163b599416f37ae69d2040
David William Fitzgerald <Dankerprouduct2@gmail.com>
2017-01-09 16:39:10 -0500

    Initial setup

---
+++ Derpbots2/Derpbots2/Map.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Derpbots2
+{
+    public class Map
+    {
+        public Map()
+        {
+            
+        }
+        public void LoadContent()
+        {
+
+        }
+        public void Update(ContentManager content)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
